00148be6b2b43a77cde4761d543239a9ed945c60
Rod_dev <95889654+rodrigoSouza1984@users.noreply.github.com>
2025-07-26 08:40:30 -0300

    Update BancoDeDados.js

--- bancoDadosConfig/BancoDeDados.js
+++ bancoDadosConfig/BancoDeDados.js
@@ -6,7 +6,7 @@ async function connect() {
     }
 
     const pool = new pg.Pool({
-        connectionString: "postgres://zezxeiuu:2hubq3AVRr4d3zR818Qrz6K_NvpmyQDI@ruby.db.elephantsql.com/zezxeiuu"
+        connectionString: "add string postgres conection - postgres://...."
     });
     
     global.connection = pool;
